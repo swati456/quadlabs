270f438f0e0b53800bae7a9766495833e1a28e38
Swati.Ahuja <Swati.Ahuja@Swati.quadlabs>
2019-05-23 17:45:41 +0530

    pushed

---
+++ Quadlabs/src/main/java/B2C/Quadlabs/App.java
@@ -0,0 +1,13 @@
+package B2C.Quadlabs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
